124c3c1fb7fcd0eec30dede5627706463587bb83
Kjartan Már Andersen <svidakjammi@gmail.com>
2022-12-16 20:23:52 +0000

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// App.tsx
+import React from "react";
+
+import './App.css'
+import CV from "./CV/CV";
+
+
+
+const App = () => (
+  <div>
+    <h1 className="h1app">Hello React</h1>
+    <CV />
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+// webpack.config.js
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle.js'
+    },
+    devServer: {
+      static: {       
+        directory: path.resolve(__dirname, './dist')
+      },
+      port: 3000,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js|ts)x?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            },
+          },
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+      },
+      resolve: {
+        extensions: [ '.ts', '.tsx', '.jsx', '.js'],
+      },
+      
+  }
