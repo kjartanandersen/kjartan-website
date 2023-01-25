6f61c26171af3125e6cd13414b42277f68ca150c
Kjartan Már Andersen <svidakjammi@gmail.com>
2023-01-25 18:14:03 +0000

    added circleci to netlify deployment

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/i,
+        use: ['file-loader']
       }
     ],
   },
