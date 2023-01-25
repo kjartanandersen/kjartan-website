4a1ecdc0f8fad25342fa30fa4d6a8f0dfab9018c
Kjartan Már Andersen <svidakjammi@gmail.com>
2023-01-25 19:31:27 +0000

    fix issues

--- src/Db/profileData.ts
+++ src/Db/profileData.ts
@@ -13,7 +13,7 @@ export const profileData: ProfileProp = {
         "Video Games",
         "Movies",
         "Music",
-        "Compuers",
+        "Computers",
         "Football"
     ],
     "languages": [
